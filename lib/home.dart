9627a1999a662226fa2b58b86c12f191e708b2ab
AutoTau <zenbenjam@gmail.com>
2021-05-30 00:46:17 -0700

    started working on home page

--- lib/home.dart
+++ lib/home.dart
@@ -9,6 +9,25 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color(0x004242),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(height: 50),
+          Text('Ben Portis',
+          style: TextStyle(color: Color(0x7D9E9E), fontSize: 20),
+          ),
+          SizedBox(height: 5),
+          Text('Cat and Dog Classification Application',
+              style: TextStyle(color: Color(0x7D9E9E),
+                  fontWeight: FontWeight.w500,
+                  fontSize: 30),
+          ),
+          SizedBox(height: 50),
+        ])
+      )
+    );
   }
 }
