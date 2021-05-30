8fb6de42bb4fe959d47db2353751ec1b76f43af0
AutoTau <zenbenjam@gmail.com>
2021-05-30 01:16:40 -0700

    Updated home page with stylings

--- lib/home.dart
+++ lib/home.dart
@@ -1,33 +1,86 @@
 import 'package:flutter/material.dart';
 
-
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  bool _loading = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0x004242),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 24),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          SizedBox(height: 50),
-          Text('Ben Portis',
-          style: TextStyle(color: Color(0x7D9E9E), fontSize: 20),
-          ),
-          SizedBox(height: 5),
-          Text('Cat and Dog Classification Application',
-              style: TextStyle(color: Color(0x7D9E9E),
-                  fontWeight: FontWeight.w500,
-                  fontSize: 30),
-          ),
-          SizedBox(height: 50),
-        ])
-      )
-    );
+        backgroundColor: Colors.blueAccent,
+        body: Container(
+            padding: EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(height: 50),
+                  Text(
+                    'Ben Portis',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    'Cat and Dog Classification Application',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 30),
+                  ),
+                  SizedBox(height: 50),
+                  Center(
+                    child: _loading
+                        ? Container(
+                            width: 350,
+                            child: Column(
+                              children: <Widget>[
+                                Image.asset('assets/cat_dog_icon.png'),
+                                SizedBox(height: 50),
+                              ],
+                            ))
+                        : Container(),
+                  ),
+                  Container(
+                      width: MediaQuery.of(context).size.width,
+                      child: Column(children: <Widget>[
+                        GestureDetector(
+                          onTap: () {},
+                          child: Container(
+                            width: MediaQuery.of(context).size.width - 250,
+                            alignment: Alignment.center,
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 18),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(6),
+                            ),
+                            child: Text(
+                              'Take a photo',
+                              style: TextStyle(color: Colors.black, fontSize: 16),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        GestureDetector(
+                          onTap: () {},
+                          child: Container(
+                            width: MediaQuery.of(context).size.width - 250,
+                            alignment: Alignment.center,
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 18),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(6),
+                            ),
+                            child: Text(
+                              'Select a photo',
+                              style: TextStyle(color: Colors.black, fontSize: 16),
+                            ),
+                          ),
+                        ),
+                      ]))
+                ])));
   }
 }
