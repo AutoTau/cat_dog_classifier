9644f7bcc98d0de80227177255654886ad1d269d
AutoTau <zenbenjam@gmail.com>
2021-05-30 04:37:55 -0700

    Now working and running on the generated model from TeachableMachine

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:tflite/tflite.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -7,6 +12,65 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   bool _loading = true;
+  late File _image;
+  late List _output;
+  final picker = ImagePicker();
+
+  @override
+  void initState() {
+    super.initState();
+    loadModel().then((value) {
+      setState(() {});
+    });
+  }
+
+  dectectImage(File image) async {
+    var output = await Tflite.runModelOnImage(
+        path: image.path,
+        numResults: 2,
+        threshold: 0.6,
+        imageMean: 127.5,
+        imageStd: 127.5,
+    );
+    setState(() {
+      _output = output!;
+      _loading = false;
+    });
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+        model: 'assets/model_unquant.tflite', labels: 'assets/labels.txt');
+  }
+
+  @override
+  void dispose() {
+    Tflite.close();
+    super.dispose();
+  }
+
+  pickImage() async {
+    var image = await picker.getImage(source: ImageSource.camera);
+    if (image == null) return null;
+
+    setState(() {
+      _image = File(image.path);
+    });
+
+    dectectImage(_image);
+  }
+
+  pickGalleryImage() async {
+    var image = await picker.getImage(source: ImageSource.gallery);
+    if (image == null) return null;
+
+    setState(() {
+      _image = File(image.path);
+    });
+
+    dectectImage(_image);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,73 +78,100 @@ class _HomeState extends State<Home> {
         body: Container(
             padding: EdgeInsets.symmetric(horizontal: 24),
             child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  SizedBox(height: 50),
-                  Text(
-                    'Ben Portis',
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                  SizedBox(height: 5),
-                  Text(
-                    'Cat and Dog Classification Application',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 30),
-                  ),
-                  SizedBox(height: 50),
-                  Center(
-                    child: _loading
-                        ? Container(
-                            width: 350,
-                            child: Column(
-                              children: <Widget>[
-                                Image.asset('assets/cat_dog_icon.png'),
-                                SizedBox(height: 50),
-                              ],
-                            ))
-                        : Container(),
-                  ),
-                  Container(
-                      width: MediaQuery.of(context).size.width,
-                      child: Column(children: <Widget>[
-                        GestureDetector(
-                          onTap: () {},
-                          child: Container(
-                            width: MediaQuery.of(context).size.width - 250,
-                            alignment: Alignment.center,
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 10, vertical: 18),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(6),
-                            ),
-                            child: Text(
-                              'Take a photo',
-                              style: TextStyle(color: Colors.black, fontSize: 16),
-                            ),
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 50),
+                Text(
+                  'AutoTau',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                SizedBox(height: 5),
+                Text(
+                  'Cat and Dog Classification Application',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 30),
+                ),
+                SizedBox(height: 50),
+                Center(
+                  child: _loading
+                      ? Container(
+                          width: 350,
+                          child: Column(
+                            children: <Widget>[
+                              Image.asset('assets/cat_dog_icon.png'),
+                              SizedBox(height: 50),
+                            ],
+                          ),
+                        )
+                      : Container(
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                height: 250,
+                                child: Image.file(_image),
+                              ),
+                              SizedBox(height: 20),
+                              _output != null
+                                  ? Text(
+                                      '${_output[0]['label']}',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 15),
+                                    )
+                                  : Container(),
+                              SizedBox(height: 10),
+                            ],
                           ),
                         ),
-                        SizedBox(height: 5),
-                        GestureDetector(
-                          onTap: () {},
-                          child: Container(
-                            width: MediaQuery.of(context).size.width - 250,
-                            alignment: Alignment.center,
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 10, vertical: 18),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(6),
-                            ),
-                            child: Text(
-                              'Select a photo',
-                              style: TextStyle(color: Colors.black, fontSize: 16),
-                            ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: () {
+                          pickImage();
+                        },
+                        child: Container(
+                          width: MediaQuery.of(context).size.width - 250,
+                          alignment: Alignment.center,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 18),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(6),
+                          ),
+                          child: Text(
+                            'Capture a Photo',
+                            style: TextStyle(color: Colors.black, fontSize: 16),
                           ),
                         ),
-                      ]))
-                ])));
+                      ),
+                      SizedBox(height: 5),
+                      GestureDetector(
+                        onTap: () {
+                          pickGalleryImage();
+                        },
+                        child: Container(
+                          width: MediaQuery.of(context).size.width - 250,
+                          alignment: Alignment.center,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 18),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(6),
+                          ),
+                          child: Text(
+                            'Select a photo',
+                            style: TextStyle(color: Colors.black, fontSize: 16),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )));
   }
 }
